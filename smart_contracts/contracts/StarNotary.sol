f69cc289480f8b43aed86742d975744b8d11b508
Abdullah Albannay <aabannay@gmail.com>
2018-12-15 16:52:53 +0300

    edited star struct

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -5,7 +5,11 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 contract StarNotary is ERC721 { 
 
     struct Star { 
-        string name; 
+        string name;
+        string starStory;
+        string ra;
+        string dec;
+        string mag;
     }
 
     mapping(uint256 => Star) public tokenIdToStarInfo; 
