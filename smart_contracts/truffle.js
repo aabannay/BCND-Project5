8f2e7cbf4745c1c58d5641fcc1bb13818588635c
Abdullah Albannay <aabannay@gmail.com>
2018-12-21 19:12:22 +0300

    added abi to the index.html, this abi is taken from build/contracts folder under the same contract file name

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -12,20 +12,23 @@
  *   },
  */
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
    // See <http://truffleframework.com/docs/advanced/configuration>
    // to customize your Truffle configuration!
    networks: {
      development: {
-       host: "localhost",
-       port: 8545,
-       network_id: "*" // Match any network id
+       	host: "localhost",
+      	port: 8545,
+       	network_id: "*" // Match any network id
    },
      rinkeby: {
-       host: "localhost", //local node
-       port: 8545, // connection port
-       network_id: 4, // network id for test networks
-       gas: 4700000 // gas limit
+       	provider: function() {
+ 			return new HDWalletProvider("chalk any fine raccoon adjust join doll insect whip gospel photo wish", "https://rinkeby.infura.io/v3/9686c812fd2b427090fe8a215dc688a4")
+     	},
+       	network_id: 4, // network id for test networks
+       	gas: 4700000 // gas limit
      }
    }
  };
