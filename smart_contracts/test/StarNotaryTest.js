e45e60a8cd3b9463c2cdbfb36eb8071b816c1bed
Abdullah Albannay <aabannay@gmail.com>
2018-12-21 15:51:52 +0300

    added operator approval test

--- smart_contracts/test/StarNotaryTest.js
+++ smart_contracts/test/StarNotaryTest.js
@@ -198,5 +198,21 @@ contract('StarNotary', accounts => {
             assert.equal(tx.logs[0].event, 'Approval')
         })
     })
+
+    describe('can set an operator', () => { 
+        let tokenId = 1
+        let tx 
+
+        beforeEach(async function () { 
+            await this.contract.mint(user1, tokenId, {from: user1})
+
+            tx = await this.contract.setApprovalForAll(operator, true, {from: user1})
+        })
+
+        it('can set an operator', async function () { 
+           //check if the operator is approved to transfer all what user1 has
+           assert.isTrue(await this.contract.isApprovedForAll(user1, operator))
+        })
+    })
  
 })
